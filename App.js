772561d83081a205b3c9f22427f86cdadde27b79
Felipe Rocha <felipe.viega.r@gmail.com>
2023-06-11 18:29:16 -0300

    Primeiros Passos

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-export default function App() {
+const Greeting = ({name}) => {
   return (
-    <View style={styles.container}>
-      <Text>TESTE APP!</Text>
-      <StatusBar style="auto" />
+    <View style={{ alignItems: 'center'}}>
+      <Text style={{ fontSize: 25}}>Hello {name}!</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function LotsOfGreetings() {
+  return (
+    <View style={{ alignItems: 'center', top: 100}} >
+      <Greeting name="Deus é Fiel"/>
+      <Greeting name="Jesus Salva"/>
+
+    </View>
+  )
+}
